64fb82566909019a41f55a707a1abc77bcbd15d4
MrNickell <songtianou07@gmail.com>
2017-11-05 01:02:24 +0800

    update Assignment_3

--- Assignment_3/main.cpp
+++ Assignment_3/main.cpp
@@ -1,17 +1,84 @@
-//
-//  main.cpp
-//  勇闯迷宫游戏
-//
-//  Created by MrNickel on 2017/10/24.
-//  Copyright © 2017年 MrNickel. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
 using namespace std;
-char 
+
+struct Position{
+    int x;
+    int y;
+};
 
 
-int main(int argc, const char * argv[]) {
+char maze[100][100] = {
+        "#######",
+        "#0#000#",
+        "#0#0###",
+        "#000#0#",
+        "#0#000#",
+        "#0#0#0#",
+        "#######",
+};
+
+
+Position direction[4] ={{0,1},{0,-1},{-1,0},{1,0}};
+
+Position exitPos = {5,5};
+vector<Position> route;
+int tag = 0;
+void dfs(int x,int y){
+    if(x ==  exitPos.x && y == exitPos.y){
+        tag = 1;
+        route.push_back(exitPos);
+        return;
+    }
+
+    for(int i = 0; i < 4 ; i++)
+    {
+        if(maze[x + direction[i].x][y + direction[i].y] == '0'){
+            maze[x][y] = 'F';
+            Position cur = {x,y};
+            route.push_back(cur);
+
+            dfs(x + direction[i].x,y + direction[i].y);
+
+            if(tag == 1)
+                return ;
+
+            route.erase(route.end() - 1);
+            maze[x][y] = '0';
+        }
+
+    }
+
+}
+void printMap(){
     cout << "迷宫地图:" << endl;
-    
+    cout << "\t";
+    for(int i = 0; i < 7; i++)
+        cout << i << "列" <<"\t\t";
+    cout << endl;
+    for(int i = 0; i < 7; i++)
+    {
+        cout << i << "行" <<"\t";
+        for(int j = 0 ;  j < 7; j++)
+        {
+            cout << maze[i][j]  <<"\t\t";
+        }
+        cout << endl << endl;
+    }
+
+
+}
+
+
+
+
+
+int main() {
+    printMap();
+    dfs(1,1);
+    cout <<"\n迷宫路径:" << endl;
+    for(auto iter = route.begin(); iter != route.end() - 1;iter++)
+        cout << "<" << iter->x << "," << iter->y << "> "<< "---> ";
+    cout << "<" << (route.end() - 1)->x << "," << (route.end() - 1)->y << ">"<<endl;
+    return 0;
 }
